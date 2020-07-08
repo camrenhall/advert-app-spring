5c0ce344d2b2695c278d5f4e6715d9cd2f920f50
Camren Hall <camren.hall@vanderbilt.edu>
2020-07-08 09:59:41 -0500

    removed data.sql and schema.sql

--- src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
--- INSERT INTO user VALUES
--- (1, 1, 'pass', 'ROLE_USER', 'user'),
--- (2, 1, 'admin', 'ROLE_ADMIN', 'admin'),
--- (3, 1, 'pass', 'ROLE_BUSINESS', 'business');
---
--- INSERT INTO campaigns VALUES
--- (1, 1, 'T-Mobile', 'tech'),
--- (2, 1, 'Texas Roadhouse', 'food'),
--- (3, 1, 'Arbys', 'food');
