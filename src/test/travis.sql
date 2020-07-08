9fcce06df4e88ec6d4351be092ea5dd106ed339b
Camren Hall <camren.hall@vanderbilt.edu>
2020-07-08 09:46:04 -0500

    more sql syntax

--- src/test/travis.sql
+++ src/test/travis.sql
@@ -7,11 +7,13 @@ CREATE TABLE campaigns (
 	  id          	    int 	    		auto_increment			PRIMARY KEY,
 	  active    	    BOOLEAN    			NOT NULL,
 	  name              VARCHAR(255)        NOT NULL,
-	  type_label        VARCHAR(255)        NOT NULL
+	  type_label        VARCHAR(255)        NOT NULL,
+	  number_assigned   int                 DEFAULT 0,
+	  number_completed  int                 DEFAULT 0
 
 );
 
-INSERT INTO campaigns VALUES
+INSERT INTO campaigns (id, active, name, type_label, number_assigned, number_completed) VALUES
 (1, 1, 'T-Mobile', 'tech'),
 (2, 1, 'Texas Roadhouse', 'food'),
 (3, 1, 'Arbys', 'food');
@@ -29,7 +31,7 @@ CREATE TABLE user (
 
 );
 
-INSERT INTO user VALUES
+INSERT INTO user (id, active, password, roles, user_name, advert_one, advert_two, advert_three) VALUES
 (1, 1, 'pass', 'ROLE_USER', 'user', 0, 0, 0),
 (2, 1, 'admin', 'ROLE_ADMIN', 'admin', 0, 0, 0),
 (3, 1, 'pass', 'ROLE_BUSINESS', 'business', 0, 0, 0);
