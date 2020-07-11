0cf8189805fffa2dff15996b99294c196cb03c5f
Camren Hall <camren.hall@vanderbilt.edu>
2020-07-10 20:40:12 -0500

    still working out tests

--- src/test/java/io/javabrains/springsecurityjpa/SpringSecurityJpaApplicationTests.java
+++ src/test/java/io/javabrains/springsecurityjpa/SpringSecurityJpaApplicationTests.java
@@ -50,7 +50,7 @@ public class SpringSecurityJpaApplicationTests {
 				perform(get("/admin/new_campaign?name=mytestcamp&type=food%27%29%3B+INSERT+INTO+campaigns+" +
 						"%28%60name%60%2C+%60type_label%60%29+VALUES+%28%27thisisaninjection%27%2C+%27food")
 						.accept(MediaType.TEXT_HTML_VALUE))
-				.andExpect(status().isNotAcceptable())
+				.andExpect(status().isAccepted())
 				.andExpect(content().contentType("text/html;charset=UTF-8"))
 				.andReturn().getResponse().getContentAsString();
 	}
